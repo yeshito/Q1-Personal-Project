ad566e52f50b8fb6ad52e12fbd4d0104eebfe881
Yeshe Wingerd <yeshito@gmail.com>
2016-05-17 21:48:55 -0700

    progress

--- script.js
+++ script.js
@@ -5,30 +5,109 @@ $( document ).ready(function() {
   $('#countries').hide();
   $('#redVarieties').hide();
   $('#whiteVarieties').hide();
+  hideFinalSelectors();
 
   var zipcode = '';
   var wineType = '';
   var countryVariety = '';
+  var variety = '';
+  var country = '';
+  var maxPrice = '';
 
-  $('#zipSubmit').click(function() {
+  $('#zipSubmit').mouseup(function() {
     zipcode = $('#zipcode').val();
+    console.log(zipcode);
     $('#wineType').fadeIn();
-    $('#zipSubmit').fadeOut();
+  });
+
+  $('#wineType label').mouseup(function(e) {
+    wineType = $(e.target).attr('for');
     $('#countryVariety').hide();
     $('#countries').hide();
     $('#redVarieties').hide();
-  })
+    $('#whiteVarieties').hide();
+    hideFinalSelectors();
+    console.log(wineType);
 
-  $('#wineType').click(function(e) {
-    wineType = $(e.target).val();
-    $('#countryVariety').fadeIn();
-  })
-  //   if (wineType === "red") {
-  //     $('#redVarieties').fadeIn();
-  //   }
-  // })
+    if(wineType === 'red' || wineType === 'white') {
+      $('#countryVariety').fadeIn();
 
+      $('#countryVariety label').mouseup(function(e) {
+        $('#redVarieties').hide();
+        $('#whiteVarieties').hide();
+        $('#countries').hide();
+        hideFinalSelectors();
+        countryVariety = $(e.target).attr('for');
+        console.log(countryVariety);
 
+          if(countryVariety === 'variety' && wineType === 'red') {
+            $('#redVarieties').fadeIn();
+            $('#countries').hide();
+            $('#whiteVarieties').hide();
+            hideFinalSelectors();
+
+            $('#redVarieties label').mouseup(function(e) {
+              $('#countries').hide();
+              $('#whiteVarieties').hide();
+              hideFinalSelectors();
+              variety = $(e.target).attr('for')
+              console.log(variety);
+              priceSubmitForm();
+            });
+
+          } else if (countryVariety === 'variety' && wineType === 'white'){
+            $('#whiteVarieties').fadeIn();
+            $('#redVarieties').hide();
+            $('#countries').hide();
+            hideFinalSelectors();
+
+            $('#whiteVarieties label').mouseup(function(e) {
+              variety = $(e.target).attr('for')
+              console.log(variety);
+              priceSubmitForm();
+            });
+          } else {
+            countrySelection();
+          }
+      });
+    } else {
+      $('#whiteVarieties').hide();
+      $('#redVarieties').hide();
+      $('#countryVariety').hide();
+      countrySelection();
+    };
+  });
+
+      function hideFinalSelectors () {
+        $('#maxPrice').hide();
+        $('#completeButton').hide();
+      }
+
+      function countrySelection () {
+      $('#countries').fadeIn();
+      $('#maxPrice').hide();
+      $('#completeButton').hide();
+
+        $('#countries label').mouseup(function(e) {
+          $('#maxPrice').hide();
+          country = $(e.target).attr('for')
+          console.log(country);
+          priceSubmitForm();
+        });
+      }
+
+      function priceSubmitForm () {
+      $('#maxPrice').fadeIn();
+      $('#completeButton').hide();
+
+      $('#maxPrice label').mouseup(function(e) {
+        maxPrice = $(e.target).attr('for')
+        console.log(maxPrice)
+        $('#completeButton').fadeIn();
+      });
+
+      $('#completeButton').mouseup(function() {});
+      }
 });
 
 
@@ -38,14 +117,6 @@ $( document ).ready(function() {
 
 
 
-  // $('#countryVariety').click(function(e) {
-  //     countryVariety = $(e.target).val();
-  //     if(countryVariety === 'country') {
-  //       $('#countries').fadeToggle();
-  //     } else {
-  //       $('#redVarieties').fadeToggle();
-  //     }
-  //   });
 
 
 
