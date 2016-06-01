493a95822dd1107ce2904a8fba04f1a78d442b1c
Yeshe Wingerd <yeshito@gmail.com>
2016-06-01 09:15:15 -0700

    functioning more button redesign some bug fixs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Q1-Personal-Project
+The task of navigating shelf after shelf of confusing wine labels, can be daunting for most wine consumers who just want to find a bottle to enjoy. After finally picking a bottle, the results can be disappointing. I created this app as a way to make the process of finding a bottle of wine that you enjoy at your price point an easy and enjoyable process.
+
+Technologies used:
+HTML5
+CSS3
+Javascript
+JQuery 2.2.4
+Materialize
--- script.js
+++ script.js
@@ -8,7 +8,6 @@ $( document ).ready(function() {
   hideFinalSelectors();
 
 var wineArguments = {
-  zipcode: '',
   wineType: '',
   variety: '',
   country: '',
@@ -16,12 +15,11 @@ var wineArguments = {
 }
 
 // since this parameter is not actually used for URL request
-var countryVariety = ''
-
-
+var countryVariety = '';
+var zipcode = '';
   $('#zipSubmit').mouseup(function() {
-    wineArguments.zipcode = $('#zipcode').val();
-    console.log(wineArguments.zipcode);
+    zipcode = $('#zipcode').val();
+    console.log(zipcode);
     $('#wineType').fadeIn();
   });
 
@@ -89,6 +87,7 @@ var countryVariety = ''
       function hideFinalSelectors () {
         $('#maxPrice').hide();
         $('#completeButton').hide();
+        $('#noResults').hide();
       }
 
       function countrySelection () {
@@ -130,9 +129,9 @@ var countryVariety = ''
         function appendWineCrumbsBar () {
           var middleWineCrumb = ''
           if (wineArguments.variety) {
-            middleWineCrumb = wineArguments.variety;
+            middleWineCrumb = wineArguments.variety.replace(/\+/g, ' ');
           } else {
-            middleWineCrumb = wineArguments.country;
+            middleWineCrumb = wineArguments.country.replace(/\+/g, ' ');
           };
 
           var wineCrumbsBar = $(`
@@ -156,15 +155,13 @@ var countryVariety = ''
 
         // Takes form data and converts to URL
         for(key in wineArguments) {
-          if (wineArguments[key] && wineArguments[key] !== 'Make it rain!') {
+          if (wineArguments[key] && wineArguments[key] !== 'The sky is the limit!') {
             if(key === 'wineType') {
               if (wineArguments[key] === 'sparkling' || wineArguments[key] === 'dessert') {
                 urlInput += `&t=${wineArguments[key]}`;
               } else {
                 urlInput += `&t=wine&color=${wineArguments[key]}`;
               }
-            } else if (key === 'zipcode') {
-              urlInput += `&z=${wineArguments[key]}`;
             } else if (key === 'maxPrice') {
               urlInput += `&xp=${wineArguments[key]}`
             } else {
@@ -173,7 +170,8 @@ var countryVariety = ''
           }
         }
 
-        var finalURLRequest = `http://api.snooth.com/wines/?akey=977mbzz45u7unhx1vg0fs4iw9r8wpzmpxm78d1yf89dhueit&n=100&c=US&lang=en&s=sr${urlInput}`
+        var finalURLRequest = `http://api.snooth.com/wines/?akey=977mbzz45u7unhx1vg0fs4iw9r8wpzmpxm78d1yf89dhueit&n=20&c=US&lang=en&s=sr${urlInput}`
+        console.log(finalURLRequest);
         var wineResultsArray = []
 
         // initial ajax request
@@ -185,30 +183,36 @@ var countryVariety = ''
             var numReturnedWines = data.meta.returned;
             console.log('returned wines ' + numReturnedWines);
 
-            if (numReturnedWines < 4) {
-              wineResultsArray = data.wines;
-            } else if (numReturnedWines <= 30) {
-              selectThreeWines(numReturnedWines);
+            if (!numReturnedWines) {
+              $('#noResults').fadeIn();
+              $('.container').remove('.row:last-child')
             } else {
-              selectThreeWines(30)
+
+              if (numReturnedWines < 4) {
+                wineResultsArray = data.wines;
+              } else if (numReturnedWines <= 30) {
+                selectThreeWines(numReturnedWines);
+              } else {
+                selectThreeWines(30)
+              }
+              console.log(wineResultsArray);
+
+              createWineCardRow(wineResultsArray);
+
+              function selectThreeWines (winesAmount) {
+                var previousChoices = [];
+
+                do {
+                  var currentWineChoice = Math.floor(Math.random() * winesAmount)
+                  if (previousChoices.indexOf(currentWineChoice) === -1) {
+                    wineResultsArray.push(data.wines[currentWineChoice]);
+                    previousChoices.push(currentWineChoice);
+                  }
+                } while (wineResultsArray.length < 3);
+                console.log(previousChoices);
+              }
             }
-            console.log(wineResultsArray);
-
-            createWineCardRow(wineResultsArray);
-
-            function selectThreeWines (winesAmount) {
-              var previousChoices = [];
-
-              do {
-                var currentWineChoice = Math.floor(Math.random() * winesAmount)
-                if (previousChoices.indexOf(currentWineChoice) === -1) {
-                  wineResultsArray.push(data.wines[currentWineChoice]);
-                  previousChoices.push(currentWineChoice);
-                }
-              } while (wineResultsArray.length < 3);
-              console.log(previousChoices);
-            }
-          },
+            },
           error: function(errorObject, textStatus) {
               console.log(errorObject);
               console.log(textStatus);
@@ -230,12 +234,18 @@ var countryVariety = ''
 
     function createBottle(wineObject) {
       var wineSearchName = wineObject.name.replace(/ /g, '+');
+      var cardImage = '';
+
+      // creates image div only if there is an image url returned
+      if (wineObject.image) {
+        cardImage = `<div class="card-image waves-effect waves-block waves-light">
+                          <img src=${wineObject.image}>
+                         </div>`;
+      }
 
       return $(`<div class="col l4">
         <div class="card hoverable">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img src=${wineObject.image}>
-        </div>
+        ${cardImage}
          <div class="card-content paper1">
                <span class="card-title grey-text text-darken-4">${wineObject.name}</span>
                  <p>Region: ${wineObject.region}</p>
@@ -245,14 +255,13 @@ var countryVariety = ''
                  <p>Price: $${wineObject.price}</p>
                  <p>Snoothrank: ${wineObject.snoothrank}</p>
                <div class="card-action">
-                 <a target="_blank" href="http://www.wine-searcher.com/find/${wineSearchName}/1/usa-${wineArguments.zipcode}-20?Xlist_format=&Xbottle_size=all&Xprice_set=CUR&Xprice_min=&Xprice_max=&Xshow_favourite=">Buy</a>
+                 <a target="_blank" href="http://www.wine-searcher.com/find/${wineSearchName}/1/usa-${zipcode}-100?Xlist_format=&Xbottle_size=all&Xprice_set=CUR&Xprice_min=&Xprice_max=&Xshow_favourite=">Buy</a>
                  <a class="activator" id=${wineObject.code}>More</a>
                </div>
          </div>
          <div class="card-reveal paper2">
            <span class="card-title grey-text text-darken-4">${wineObject.name}<i class="material-icons right">close</i></span>
-           <a target="_blank" href="http://www.wine-searcher.com/find/${wineSearchName}/1/usa-${wineArguments.zipcode}-20?Xlist_format=&Xbottle_size=all&Xprice_set=CUR&Xprice_min=&Xprice_max=&Xshow_favourite=">Buy</a>
-           <p></p>
+           <a target="_blank" href="http://www.wine-searcher.com/find/${wineSearchName}/1/usa-${zipcode}-100?Xlist_format=&Xbottle_size=all&Xprice_set=CUR&Xprice_min=&Xprice_max=&Xshow_favourite=">Buy</a>
          </div>
         </div>
         </div>`);
@@ -263,8 +272,10 @@ var countryVariety = ''
 
       $('.activator').one('click', function(e) {
         var bottleID = this.id;
+        var divToPopulate = $(this).parent().parent().next();
+        console.log(this);
         console.log(bottleID);
-        var bottleURL = `http://api.snooth.com/wine/?akey=977mbzz45u7unhx1vg0fs4iw9r8wpzmpxm78d1yf89dhueit&food=1&lang=en&c=US&z=${wineArguments.zipcode}&id=${bottleID}`
+        var bottleURL = `http://api.snooth.com/wine/?akey=977mbzz45u7unhx1vg0fs4iw9r8wpzmpxm78d1yf89dhueit&food=1&lang=en&id=${bottleID}`
         console.log(bottleURL)
 
         $.ajax({
@@ -272,7 +283,9 @@ var countryVariety = ''
           method: "GET",
           success: function(data) {
             data = JSON.parse(data)
-            console.log(data);
+            var wineryNotes = data.wines[0].winery_tasting_notes;
+            var wineMakerNotes = data.wines[0].wm_notes;
+            $(divToPopulate).append(`<p>${wineryNotes}${wineMakerNotes}</p>`)
           },
           error: function(errorObject, textStatus) {
               console.log(errorObject);
